33ef90aa47962c41abf303fb5591862019fbf102
Cameron Miller <camm73@yahoo.com>
2020-05-16 15:00:36 -0700

    Changed format

--- barbotSkill.py
+++ barbotSkill.py
@@ -1,24 +1,24 @@
-import json
-import boto3
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_model import Response
+from ask_sdk_model.ui import SimpleCard
+from ask_sdk_core.skill_builder import SkillBuilder
 
-iotClient = boto3.client('iot-data', region_name='us-east-1')
+sb = SkillBuilder()
 
-def lambda_handler(event, context):
+@sb.request_handler(can_handle_func=lambda handler_input:
+    is_intent_name('MakeCocktail'))
+def make_cocktail_handler(handler_input):
+    if('cocktail' in handler_input.attributes_manager.session_attributes):
+        cocktail = handler_input.attributes_manager.session_attributes['cocktail']
+        speech = "Making your {} cocktail.".format(cocktail)
+        handler_input.response_builder.set_should_end_session(True)
+    else:
+        speech = "I was unable to understand what cocktail you requested"
 
-    cocktailType = event['request']['intent']['slots']['cocktail']['value']
+    handler_input.response_builder.speak(speech)
+    return handler_input.response_builder.response
 
-    params = {
-        "action": 'makeCocktail',
-        "data" : cocktailType.lower()
-    }
 
-    response = iotClient.publish(
-        topic='barbot-main',
-        qos=1,
-        payload=json.dumps(params)
-    )
-
-    return{
-        'statusCode': 200,
-        'body': response
-    }
+lambda_handler = sb.lambda_handler
