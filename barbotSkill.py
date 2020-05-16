1e490b8b379c225b3e17e8d24292f60fb90b8095
Cameron Miller <camm73@yahoo.com>
2020-05-16 15:16:32 -0700

    Added IoT integration

--- barbotSkill.py
+++ barbotSkill.py
@@ -6,6 +6,9 @@ from ask_sdk_model.ui import SimpleCard
 from ask_sdk_core.skill_builder import SkillBuilder
 
 import boto3
+import json
+
+iotClient = boto3.client('iot-data', region_name='us-east-1')
 
 sb = SkillBuilder()
 
@@ -15,6 +18,19 @@ def make_cocktail_handler(handler_input):
         cocktail = handler_input.request_envelope.request.intent.slots['cocktail'].value
         speech = "Making your {} cocktail.".format(cocktail)
         handler_input.response_builder.set_should_end_session(True)
+
+        iotPayload = {
+            "action": "makeCocktail",
+            "data": cocktail.lower()
+        }
+
+        iotResponse = iotClient.publish(
+            topic='barbot-main',
+            qos=1,
+            payload=json.dumps(iotPayload)
+        )
+
+        print(iotResponse)
     else:
         speech = "I was unable to understand what cocktail you requested"
 
