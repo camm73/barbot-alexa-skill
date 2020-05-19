a6342c3a5af7aad60c45ef7f517397e519b5a7a4
Cameron Miller <camm73@yahoo.com>
2020-05-18 21:51:36 -0700

    Added confirmation

--- barbotSkill.py
+++ barbotSkill.py
@@ -1,7 +1,7 @@
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
 from ask_sdk_core.handler_input import HandlerInput
-from ask_sdk_model import Response
+from ask_sdk_model import Response, IntentConfirmationStatus
 from ask_sdk_model.ui import SimpleCard
 from ask_sdk_core.skill_builder import SkillBuilder
 
@@ -17,21 +17,27 @@ def make_cocktail_handler(handler_input):
     if('cocktail' in handler_input.request_envelope.request.intent.slots):
         cocktail = handler_input.request_envelope.request.intent.slots['cocktail'].value
         print(handler_input.request_envelope.request.intent.confirmation_status)
-        speech = "Making your {} cocktail.".format(cocktail)
-        handler_input.response_builder.set_should_end_session(True)
+        confirmationStatus = handler_input.request_envelope.request.intent.confirmation_status
 
-        iotPayload = {
-            "action": "makeCocktail",
-            "data": cocktail.lower()
-        }
+        if(confirmationStatus == IntentConfirmationStatus.CONFIRMED):
+            speech = "Making your {} cocktail.".format(cocktail)
+            handler_input.response_builder.set_should_end_session(True)
 
-        iotResponse = iotClient.publish(
-            topic='barbot-main',
-            qos=1,
-            payload=json.dumps(iotPayload)
-        )
+            iotPayload = {
+                "action": "makeCocktail",
+                "data": cocktail.lower()
+            }
 
-        print(iotResponse)
+            iotResponse = iotClient.publish(
+                topic='barbot-main',
+                qos=1,
+                payload=json.dumps(iotPayload)
+            )
+
+            print(iotResponse)
+        else:
+            speech = "OK. Ask me again when you are ready to make your cocktail."
+            handler_input.response_builder.set_should_end_session(True)
     else:
         speech = "I was unable to understand what cocktail you requested"
 
